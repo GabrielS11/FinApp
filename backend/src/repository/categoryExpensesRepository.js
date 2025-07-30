618e7b8dda5a9cb1f6a6c5c30d5857c15cf7c3ab
GabrielS11 <gabrielrsab@gmail.com>
2025-07-30 16:19:38 +0100

    Made changes to adjust the DB

---
+++ backend/src/repository/categoryExpensesRepository.js
@@ -0,0 +1,7 @@
+import prisma from "../prismaClient.js";
+
+/**
+ * Adicionar as categorias, caso ja exista adicionar so o valor
+ * caso de delete ou update tambem atualizar
+ * e depois adicionar o endpoint de GET categorias
+ */
